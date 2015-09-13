913d2caaabfc1521a7817401ee510fa257ab14a9
Roman <roman.korneyev@gmail.com>
2015-09-13 19:09:36 +0300

    improve

--- server/src/routes/athletes.ts
+++ server/src/routes/athletes.ts
@@ -4,7 +4,6 @@ var router = express.Router();
 
 /* GET current athlete. */
 router.get('/', function(req: express.Request, res: express.Response, next: Function) {
-  res.status(200);
   res.send('Roman');
 });
 
