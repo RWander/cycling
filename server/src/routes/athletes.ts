170a2c0813f481c457b77dbf457cf1b8063d9d9d
Roman <roman.korneyev@gmail.com>
2015-09-14 14:07:44 +0300

    use ES5 target fot tsc, request Strava API

--- server/src/routes/athletes.ts
+++ server/src/routes/athletes.ts
@@ -1,10 +1,16 @@
 import express = require('express');
+import config = require('config');
+import request = require('request-promise');
 
 var router = express.Router();
 
 /* GET current athlete. */
-router.get('/', function(req, res, next) {
-  res.send('Roman');
+router.get('/', (req, res, next) => {
+  var strava = config.get<any>('strava');
+  request(strava.url + 'athlete?access_token=' + strava.accessToken)
+    .then((body) => res.send(body))
+    .catch((err) => next(err)
+  );
 });
 
 export = router;
