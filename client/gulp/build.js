c0ec6cfc1adbd2f9bebf8da15a99caddfabd8717
Roman <roman.korneyev@gmail.com>
2015-09-18 19:04:19 +0300

    theme

--- client/gulp/build.js
+++ client/gulp/build.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp-help')(require('gulp'));
 var runSequence = require('run-sequence');        // https://github.com/OverZealous/run-sequence
+var debug = require('gulp-debug');
+var path = require('./.path.json');
 
 // This will run in this order:
 // * clean
@@ -8,7 +10,14 @@ var runSequence = require('run-sequence');        // https://github.com/OverZeal
 // * Finally call the callback function
 gulp.task('build', 'Builds the site.', function(cb) {
   runSequence('clean',
-    ['ts'/*, 'scss'*/],
+    ['ts'/*, 'scss'*/, 'static'],
     'jade',
     cb);
+});
+
+// TODO (Roman) - use ts, scss task instead of this task.
+gulp.task('static', 'Move static files in the dist folder.', function() {
+  gulp.src(path.src.theme)
+    .pipe(debug({ title: 'static:'}))
+    .pipe(gulp.dest(path.dist.html));
 });
