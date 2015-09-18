98a8a94ee2c2f97bfbc6adb9673216f0148890c1
Roman <roman.korneyev@gmail.com>
2015-09-18 12:41:01 +0300

    config file for gulp (.path.json)

--- client/gulp/jade.js
+++ client/gulp/jade.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp-help')(require('gulp'));
 var jade = require('gulp-jade');
 var debug = require('gulp-debug');
+var path = require('./.path.json');
 
 gulp.task('jade', 'Compiles all jade templates', function() {
-  var LOCALS = {};
-
-  gulp.src('./src/*.jade')
+  gulp.src(path.src.jade)
     .pipe(debug({ title: 'jade-compiled:'}))
     .pipe(jade())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(path.dist.html));
 });
