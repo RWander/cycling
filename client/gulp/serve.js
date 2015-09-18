2f9df6db6f436b59aaccd07171f88e07257fb9bf
Roman <roman.korneyev@gmail.com>
2015-09-18 16:40:12 +0300

    gulp/server,js

--- client/gulp/serve.js
+++ client/gulp/serve.js
@@ -1,2 +1,11 @@
-// TODO (Roman)
-// ..
+var gulp = require('gulp-help')(require('gulp'));
+var browserSync = require('browser-sync').create();
+var path = require('./.path.json');
+
+gulp.task('serve', 'Runs the development server', ['build', 'watch'], function () {
+  browserSync.init({
+      server: {
+          baseDir: path.dist.root
+      }
+  });
+});
