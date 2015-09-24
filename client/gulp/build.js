c52d25927fe920f0030756dec1fb81061c8f50db
Roman <roman.korneyev@gmail.com>
2015-09-24 16:55:31 +0300

    gulp task build:scripts = build:tsconfig + build:ts + build:js-vendor

--- client/gulp/build.js
+++ client/gulp/build.js
@@ -3,14 +3,9 @@ var runSequence = require('run-sequence');        // https://github.com/OverZeal
 var debug = require('gulp-debug');
 var path = require('./.path.json');
 
-// This will run in this order:
-// * clean
-// * build *.ts, *.scss files and images in parallel
-// * build *.jade
-// * Finally call the callback function
 gulp.task('build', 'Builds the site.', function(cb) {
   runSequence('clean',
-    ['build:js', 'build:scss', 'build:img'],
+    ['build:scripts', 'build:scss', 'build:img'],
     'build:jade',
     cb);
 });
