553e108c196087e4b483c84c04288c749a1cdcf5
Roman Korneyev <roman.korneyev@gmail.com>
2015-11-22 21:02:05 +0400

    show athlete name in main page

--- client/src/ts/controllers/cycling.ts
+++ client/src/ts/controllers/cycling.ts
@@ -1,5 +1,17 @@
 module Cycling {
   export class CyclingController implements Repository {
+    // $inject annotation.
+		// It provides $injector with information about dependencies to be injected into constructor
+		// it is better to have it close to the constructor, because the parameters must match in count and type.
+		// See http://docs.angularjs.org/guide/di
+		public static $inject = [
+			'$scope'
+		];
+
+		constructor(private $scope: any) {
+      $scope.Athlete = this.get();
+		}
+
     get(): Athlete {
       return new Athlete('Роман', 'Корнеев');
     }
