899cd20a29c4687726dea3cb8f3eb7b324c3210f
Roman Korneyev <roman.korneyev@gmail.com>
2015-11-23 19:06:01 +0400

    HTTP get full info about the athlete

--- client/src/ts/controllers/cycling.ts
+++ client/src/ts/controllers/cycling.ts
@@ -1,15 +1,26 @@
 module Cycling {
+  interface ICyclingScope {
+    Athlete: Athlete;
+    FullInfo: any;
+  }
+
   export class CyclingController implements Repository {
     // $inject annotation.
 		// It provides $injector with information about dependencies to be injected into constructor
 		// it is better to have it close to the constructor, because the parameters must match in count and type.
 		// See http://docs.angularjs.org/guide/di
 		public static $inject = [
-			'$scope'
+			'$scope',
+      '$http'
 		];
 
-		constructor(private $scope: any) {
+		constructor(
+      private $scope: ICyclingScope,
+      private $http: angular.IHttpService) {
       $scope.Athlete = this.get();
+      $http.get('http://localhost:5000/').success((data: any) => {
+        $scope.FullInfo = data;
+      });
 		}
 
     get(): Athlete {
--- server/src/app.ts
+++ server/src/app.ts
@@ -13,6 +13,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // regester routes
 app.use('/', require('./routes/athletes'));
 
