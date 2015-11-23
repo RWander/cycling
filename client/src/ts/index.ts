e4601929d82046e9300f175c1d3d7ea6390b2704
Roman Korneyev <roman.korneyev@gmail.com>
2015-11-24 00:27:04 +0400

    code cleaning up

--- client/src/ts/index.ts
+++ client/src/ts/index.ts
@@ -1,6 +1,6 @@
 /// <reference path='../../typings/tsd.d.ts' />
 
 module Cycling {
-  var todomvc = angular.module('cycling', [])
+  angular.module('cycling', [])
     .controller('cyclingCtrl', CyclingController);
 }
