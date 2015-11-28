d495730a85ba92e1840b45f7b6a0de98adad2279
Roman Korneyev <roman.korneyev@gmail.com>
2015-11-28 23:10:51 +0400

    about me

--- server/src/routes/athletes.ts
+++ server/src/routes/athletes.ts
@@ -7,12 +7,24 @@ var router = express.Router();
 
 /* GET current athlete. */
 router.get('/', (req, res, next) => {
+  var info: any = config.get('me');
+  setStravaProfile(info, req, res, next);
+});
+
+function setStravaProfile(
+  info: any,
+  req: express.Request,
+  res: express.Response,
+  next: Function): void {
   var strava = config.get<any>('strava');
   var params = qs.stringify({ access_token: strava.accessToken });
 
   request(strava.url + '/athlete?' + params)
-    .then((body) => res.send(body))
-    .catch((err) => next(err));
-});
+   .then((body) => {
+     info.stravaProfile = body;
+     res.send(info);
+   })
+   .catch((err) => next(err));
+}
 
 export = router;
