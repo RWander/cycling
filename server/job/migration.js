6124e4f1f11dd9fdd8e9f2d4248e8d6ff0f3206e
Roman Korneyev <roman.korneyev@gmail.com>
2016-01-28 19:44:30 +0400

    refactoring

--- server/job/migration.js
+++ server/job/migration.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+/* eslint-disable no-console */
+
 var _ = require('lodash');
 var strava = require('strava-v3');
 var models = require('../src/models');
@@ -14,7 +16,7 @@ var currentAthlete;
 // 1. Connect db
 require('../src/db')
 // 2. Get Current Athlete
-.then(() => getCurrentAthelete())
+.then(getCurrentAthelete)
 // 3. Get all new activities of the currecnt athlete
 .then((athlete) => {
   currentAthlete = athlete;
@@ -40,16 +42,10 @@ require('../src/db')
 .then((promises) => Promise.all(promises))
 // 6. Print result.
 .then((trainings) => {
-  /* eslint-disable no-console */
   trainings.forEach((t) => console.log(`Training '${t.name}' at ${t.startDate} is saved.`));
   console.log(`Total trainings: ${trainings.length}`);
-  /* eslint-disable no-console */
 })
-.catch((err) => {
-  /* eslint-disable no-console */
-  console.log(err);
-  /* eslint-disable no-console */
-});
+.catch(console.log);
 
 /**
  * getCurrentAthelete - Loads a current athlete info.
