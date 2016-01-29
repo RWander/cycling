769ce711ad1a136b3431a62c4850a9d3f85037dc
Roman Korneyev <roman.korneyev@gmail.com>
2016-01-29 22:36:37 +0400

    calculate statistic test

--- server/test/spec/statistic.spec.js
+++ server/test/spec/statistic.spec.js
@@ -10,10 +10,55 @@ describe('Calculate statistic', function() {
     let trainings = generateTrainings();
     let stat = calculate(trainings);
 
+    // TODO: temporary failed
+    // ..
+
     expect(stat).toEqual({});
   });
 
   function generateTrainings() {
-    return [];
+    let trainings = [];
+
+    for(let i = 0; i < 10; i++) {
+      let movingTime = faker.random.number({
+        min: 1800, // 30 mins
+        max: 14400 // 4 hours
+      });
+      let elapsedTime = faker.random.number({
+        min: movingTime, // movingTime + delta mins
+        max: 14400 // 4 hours
+      });
+
+      let training = {
+        stravaId: faker.random.number(),
+        name: faker.lorem.sentence(),
+        desc: faker.lorem.sentence(),
+        type: faker.random.number({
+          min: 1,
+          max: 3
+        }),
+        startDate: faker.date.between(new Date(2014, 0, 1), new Date()),
+        athlete: {
+          // don't use
+        },
+        distance: faker.random.number({
+          min: 1000,
+          max: 30000
+        }),
+        movingTime: movingTime,
+        elapsedTime: elapsedTime,
+        elevationGain: faker.random.number({
+          min: 20,
+          max: 500
+        })
+        //averageSpeed:
+        //maxSpeed:
+      };
+      trainings.push(training);
+    }
+
+    // debugger;
+
+    return trainings;
   }
 });
