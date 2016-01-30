367727a7b8005dbf631717ddcfe77e2c4b928911
Roman Korneyev <roman.korneyev@gmail.com>
2016-01-30 23:29:36 +0400

    test infrastrucure

---
+++ server/test/spec/helpers/generator.js
@@ -0,0 +1,58 @@
+
+'use strict';
+
+/**
+ * anonymous function - Generate training objects.
+ *
+ * @param  {Number} count Count of trainings
+ * @return {Array}        Trainings
+ */
+var getTrainings = function (count) {
+  var faker = require('faker');
+
+  let trainings = [];
+
+  for(let i = 0; i < count; i++) {
+    let movingTime = faker.random.number({
+      min: 1800, // 30 mins
+      max: 14400 // 4 hours
+    });
+    let elapsedTime = faker.random.number({
+      min: movingTime, // movingTime + delta mins
+      max: 14400 // 4 hours
+    });
+
+    let training = {
+      stravaId: faker.random.number(),
+      name: faker.lorem.sentence(),
+      desc: faker.lorem.sentence(),
+      type: faker.random.number({
+        min: 1,
+        max: 3
+      }),
+      startDate: faker.date.between(new Date(2014, 0, 1), new Date()),
+      athlete: {
+        // don't use
+      },
+      distance: faker.random.number({
+        min: 1000,
+        max: 30000
+      }),
+      movingTime: movingTime,
+      elapsedTime: elapsedTime,
+      elevationGain: faker.random.number({
+        min: 20,
+        max: 500
+      })
+      //averageSpeed:
+      //maxSpeed:
+    };
+    trainings.push(training);
+  }
+
+  // debugger;
+
+  return trainings;
+};
+
+module.exports.trainings = getTrainings;
--- server/test/spec/statistic.spec.js
+++ server/test/spec/statistic.spec.js
@@ -3,11 +3,11 @@
 /* eslint-disable no-undef */
 
 describe('Calculate statistic', function() {
-  var faker = require('faker');
   var calculate = require('../../src/libs/statistic');
+  var generator = require('./helpers/generator');
 
-  it('1 is 1', function() {
-    let trainings = generateTrainings();
+  it('Check statistic', function() {
+    let trainings = generator.trainings(10);
     let stat = calculate(trainings);
 
     // TODO: temporary failed
@@ -15,50 +15,4 @@ describe('Calculate statistic', function() {
 
     expect(stat).toEqual({});
   });
-
-  function generateTrainings() {
-    let trainings = [];
-
-    for(let i = 0; i < 10; i++) {
-      let movingTime = faker.random.number({
-        min: 1800, // 30 mins
-        max: 14400 // 4 hours
-      });
-      let elapsedTime = faker.random.number({
-        min: movingTime, // movingTime + delta mins
-        max: 14400 // 4 hours
-      });
-
-      let training = {
-        stravaId: faker.random.number(),
-        name: faker.lorem.sentence(),
-        desc: faker.lorem.sentence(),
-        type: faker.random.number({
-          min: 1,
-          max: 3
-        }),
-        startDate: faker.date.between(new Date(2014, 0, 1), new Date()),
-        athlete: {
-          // don't use
-        },
-        distance: faker.random.number({
-          min: 1000,
-          max: 30000
-        }),
-        movingTime: movingTime,
-        elapsedTime: elapsedTime,
-        elevationGain: faker.random.number({
-          min: 20,
-          max: 500
-        })
-        //averageSpeed:
-        //maxSpeed:
-      };
-      trainings.push(training);
-    }
-
-    // debugger;
-
-    return trainings;
-  }
 });
