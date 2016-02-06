6bc4a7918872f3c3f1ef1e80c05502cf2c78c308
Roman Korneyev <roman.korneyev@gmail.com>
2016-02-06 20:27:45 +0400

    route refactored

--- server/src/routes/athletes.js
+++ server/src/routes/athletes.js
@@ -5,8 +5,6 @@ var jpath = require('json-path');
 var models = require('../models');
 var statistic = require('../libs/statistic');
 
-var statisticObj;
-
 var router = express.Router();
 
 /**
@@ -20,40 +18,45 @@ router.get('/', (req, res, next) => {
 
 
 /**
+ * GETs full information about athelete (including tarining log and statistic).
+ * TODO (rwander): return only recent training log.
+ */
+router.get('/full', (req, res, next) => {
+  Promise.all([
+    loadAthlete(),
+    loadTrainigns(),
+    loadStatistic()
+  ]).then(results => {
+    res.send({
+      athlete: results[0],
+      trainings: results[1],
+      statistic: results[2]
+    });
+  })
+  .catch(next);
+});
+
+/**
  * GETs a full athelete information including track history.
  */
 router.get('/activities', (req, res, next) => {
   loadTrainigns()
-    .then((trainings) => {
-      res.send(trainings);
-    })
+    .then(trainings => { res.send(trainings); })
     .catch(next);
 });
-
 
 /**
  * GETs a training statistic for the currect athlete.
  */
 router.get('/statistic', (req, res, next) => {
   // TODO (rwander): validate req.query param.
-  // ..
-  var path = req.query.path ? req.query.path : '';
+  var path = req.query.path;
 
-  if (statisticObj) {
-    let data = jpath.resolve(statisticObj, path);
-    res.send(data);
-  } else {
-    // calculate statisticObj
-    loadTrainigns()
-      .then((trainings) => {
-        statisticObj = statistic(trainings);
-
-        let data = jpath.resolve(statisticObj, path);
-        res.send(data);
-      })
-      .catch(next);
-  }
+  loadStatistic(path)
+    .then(stat => { res.send(stat); })
+    .catch(next);
 });
+
 
 /**
  * fromDB - Loads athelete from local db.
@@ -63,6 +66,7 @@ router.get('/statistic', (req, res, next) => {
 function loadAthlete() {
   return models.Athlete.loadOne({ });
 }
+
 
 /**
  * fromDB - Loads athelete's trainings from local db.
@@ -74,5 +78,26 @@ function loadTrainigns() {
     populate: false // don't load refs
   });
 }
+
+
+ /**
+  * loadStatistic - Loads athelete's statistic trainings.
+  *
+  * @param  {String}  path  Json-path string (see https://github.com/flitbit/json-path)
+  * @return {Promise}
+  */
+function loadStatistic(path) {
+  path = path ? path : '';
+  if (statisticObj) {
+    return new Promise(resolve => resolve(jpath.resolve(statisticObj, path)));
+  } else {
+    return loadTrainigns()
+      .then(trainings => {
+        statisticObj = statistic(trainings);
+        return jpath.resolve(statisticObj, path);
+      });
+  }
+}
+var statisticObj;
 
 module.exports = router;
