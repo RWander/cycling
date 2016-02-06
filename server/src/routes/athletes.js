9028e4864dbf88523329147b7aceb8772adef154
Roman Korneyev <roman.korneyev@gmail.com>
2016-02-06 17:36:19 +0400

    HTTP GET method: statistic

--- server/src/routes/athletes.js
+++ server/src/routes/athletes.js
@@ -1,43 +1,19 @@
 'use strict';
 
 var express = require('express');
-var strava = require('strava-v3');
-
-var models = require('../models');
-var athletes = models.Athlete;
-
 var jpath = require('json-path');
+var models = require('../models');
 var statistic = require('../libs/statistic');
+
 var statisticObj;
 
 var router = express.Router();
 
 /**
- * GETs a information about athelete (from local db and Strava API).
+ * GETs a information about athelete.
  */
 router.get('/', (req, res, next) => {
-  Promise.all([
-    // Load athlete info from db
-    fromDB(),
-    // Load athlete info from the Strava API
-    fromStravaAPI()
-  ])
-  .then(
-    results => {
-      let info = results[0];
-      info.stravaProfile = results[1];
-      res.send(info);
-    }
-  )
-  .catch(next);
-});
-
-
-/**
- * GETs a short information about athelete (from local db).
- */
-router.get('/short', (req, res, next) => {
-  fromDB()
+  loadAthlete()
     .then(info => res.send(info))
     .catch(next);
 });
@@ -47,8 +23,10 @@ router.get('/short', (req, res, next) => {
  * GETs a full athelete information including track history.
  */
 router.get('/activities', (req, res, next) => {
-  fromStravaAPIGetActivities()
-    .then(activities => res.send(activities))
+  loadTrainigns()
+    .then((trainings) => {
+      res.send(trainings);
+    })
     .catch(next);
 });
 
@@ -59,63 +37,41 @@ router.get('/activities', (req, res, next) => {
 router.get('/statistic', (req, res, next) => {
   // TODO (rwander): validate req.query param.
   // ..
-  var path = req.query.path;
+  var path = req.query.path ? req.query.path : '';
 
   if (statisticObj) {
     let data = jpath.resolve(statisticObj, path);
     res.send(data);
   } else {
     // calculate statisticObj
-    models.Training.loadMany({ }, {
-      populate: false // don't load refs
-    }).then((trainings) => {
-      statisticObj = statistic(trainings);
+    loadTrainigns()
+      .then((trainings) => {
+        statisticObj = statistic(trainings);
 
-      let data = jpath.resolve(statisticObj, path);
-      res.send(data);
-    }).catch(next);
+        let data = jpath.resolve(statisticObj, path);
+        res.send(data);
+      })
+      .catch(next);
   }
 });
 
 /**
- * fromDB - Loads athelete info from local db.
+ * fromDB - Loads athelete from local db.
  *
  * @return {Promise}
  */
-function fromDB() {
-  return athletes.loadOne({ });
+function loadAthlete() {
+  return models.Athlete.loadOne({ });
 }
 
-
 /**
- * fromStravaAPI - Loads athelete info from Strava API.
+ * fromDB - Loads athelete's trainings from local db.
  *
  * @return {Promise}
  */
-function fromStravaAPI() {
-  return new Promise((resolve, reject) => {
-    strava.athlete.get({ }, (err, payload) => {
-      if (err) reject(err);
-      resolve(payload);
-    });
-  });
-}
-
-
-/**
- * fromStravaAPIGetActivities - Loads athelete's activities from Strava API.
- *
- * @return {Promise}
- */
-function fromStravaAPIGetActivities() {
-  return new Promise((resolve, reject) => {
-    strava.athlete.listActivities({
-      'page': 1,
-      'per_page': 200
-    }, (err, payload) => {
-      if (err) reject(err);
-      resolve(payload);
-    });
+function loadTrainigns() {
+  return models.Training.loadMany({ }, {
+    populate: false // don't load refs
   });
 }
 
