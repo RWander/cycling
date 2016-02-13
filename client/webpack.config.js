389cc467a8682c691e2db7fc9a0d8e907361f7b4
Roman Korneyev <roman.korneyev@gmail.com>
2016-02-13 09:35:05 +0400

    webpack configuration - continue

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,21 +6,33 @@ const webpack = require('webpack');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: './src/index',
-  output: {
-    filename: './build/build.js'
+  // input
+  context: __dirname + '/src',
+  entry: {
+    index: './index'
   },
 
+  // output
+  output: {
+    path: 'public',
+    filename: './[name].js'
+  },
+
+  // watch options
   watch: NODE_ENV === 'development',
   watchOptions: {
     aggregateTimeout: 1000
   },
 
+  // Source map
   devtool: NODE_ENV === 'development' ? 'source-map' : null,
 
   plugins: [
-    // In client js code you can the NODE_ENV value via process.env.NODE_ENV.
-    new webpack.EnvironmentPlugin('NODE_ENV')
+    new webpack.EnvironmentPlugin('NODE_ENV'), // In client js code you can the NODE_ENV value via process.env.NODE_ENV.
+    new webpack.NoErrorsPlugin(), // Don't emit compiled js code if there are any errors
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
+    })
   ],
 
   resolve: {
