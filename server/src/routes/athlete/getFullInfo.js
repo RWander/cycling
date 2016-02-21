06c0f73838d37acfeedca43b81b119a42317662c
Roman Korneyev <roman.korneyev@gmail.com>
2016-02-21 22:56:42 +0400

    get short statistic

--- server/src/routes/athlete/getFullInfo.js
+++ server/src/routes/athlete/getFullInfo.js
@@ -1,11 +1,34 @@
-var Athlete = require('../../models').Athlete;
-var Training = require('../../models').Training;
+'use strict';
+
+var moment = require('moment');
+var models = require('../../models');
 
 function getFullInfo(req, res, next) {
+  let types = models.TRAINING_TYPE.vals();
+  let currentTime = moment();
+  let year = currentTime.format('YYYY');  // Current Year (2016)
+  let month = currentTime.format('MMM');  // Current Month (Feb)
+  let week = currentTime.format('WW');    // Current Week (06)
+  let day = currentTime.format('DD');     // Current Day (10)
+
+  // Путь для получения ститистики "за сегодня", "за неделю", "за месяц",
+  // "за год" и "за все время".
+  let jsonPath = `take(${
+    types
+      .map(type => `/${type}`)
+      .concat([
+        `/${year}/${month}/${week}/${day}`,
+        `/${year}/${month}/${week}`,
+        `/${year}/${month}`,
+        `/${year}`
+      ])
+      .join()
+  })`;
+
   Promise.all([
-    Athlete.loadCurrent(),
-    Training.loadCurrent(), // TODO (rwander): return only recent training log.
-    Training.loadStatisticCurrent()
+    models.Athlete.loadCurrent(),
+    models.Training.loadCurrent(),
+    models.Training.loadStatisticCurrent(jsonPath)
   ]).then(results => {
     res.send({
       athlete: results[0],
