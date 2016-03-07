15921b3926a800237c576aecf03295d192dfca22
Roman Korneyev <roman.korneyev@gmail.com>
2016-03-07 23:20:53 +0400

    Cycling, Run, Ski, Swim components

--- client/src/components/Journal/Cycling.js
+++ client/src/components/Journal/Cycling.js
@@ -13,7 +13,7 @@ export default class Cycling extends Component {
           <h4><a>{training.name}</a></h4>
           <p>
           <small title="Расстояние">р:&nbsp;</small><span><samp><strong>{training.distance}<small>км</small></strong></samp></span>&nbsp;
-          <small title="Общее время">вр:&nbsp;</small><span><samp><strong>{training.elapsedTime}<small>c</small></strong></samp></span>&nbsp;
+          <small title="Общее время">вр:&nbsp;</small><span><samp><strong>{training.elapsedTime}</strong></samp></span>&nbsp;
           <small title="Средняя скорость">ср.&nbsp;ск:&nbsp;</small><span><samp><strong>{training.averageSpeed}<small>км/ч</small></strong></samp></span>
           </p>
         </div>
@@ -28,8 +28,8 @@ Cycling.propTypes = {
     name: PropTypes.string.isRequired,
     startDate: PropTypes.string.isRequired,
     distance: PropTypes.number.isRequired,
-    movingTime: PropTypes.number.isRequired,
-    //elapsedTime: PropTypes.number.isRequired,
+    //movingTime: PropTypes.string.isRequired,
+    elapsedTime: PropTypes.string.isRequired,
     //elevationGain: PropTypes.number.isRequired,
     averageSpeed: PropTypes.number.isRequired//,
     //maxSpeed: PropTypes.number.isRequired
--- client/src/reducers/journal.js
+++ client/src/reducers/journal.js
@@ -5,11 +5,22 @@ const journal = (state = [], action) => {
   case REQUEST_FULL_INFO:
     return state;
   case RECEIVE_FULL_INFO: {
-    const { trainings } = action.data;
+    const trainings = action.data.trainings.map(convertUnits);
     return trainings;
   } default:
     return state;
   }
 };
+
+function convertUnits(training) {
+  // TODO (rwander): convertUnits
+  // ..
+
+  training.distance = Number((training.distance/1000).toFixed(1)) /*m to km*/;
+  //training.elapsedTime = Number((training.elapsedTime/360).toFixed(1)) /*c to km*/;
+  //training.distance = Number((training.distance/1000).toFixed(1)) /*m to km*/;
+
+  return training;
+}
 
 export default journal;
